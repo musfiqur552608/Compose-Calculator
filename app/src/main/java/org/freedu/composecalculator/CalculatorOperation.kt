1e431503226dca61ceac637f2d31a5ee2fa10972
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-04-28 09:41:11 +0600

    CalculatorAction & CalculatorOperation class

---
+++ app/src/main/java/org/freedu/composecalculator/CalculatorOperation.kt
@@ -0,0 +1,8 @@
+package org.freedu.composecalculator
+
+sealed class CalculatorOperation (val symbol:String){
+    object Add:CalculatorOperation("+")
+    object Subtract:CalculatorOperation("-")
+    object Multiply:CalculatorOperation("x")
+    object Divide:CalculatorOperation("/")
+}
