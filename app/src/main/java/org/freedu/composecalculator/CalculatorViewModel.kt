6545d8877a8018d5d56f4b9d55f3610b87a0d670
musfiqur552608 <mushfiqur15-10872@diu.edu.bd>
2024-04-28 10:45:36 +0600

    All calculation done

--- app/src/main/java/org/freedu/composecalculator/CalculatorViewModel.kt
+++ app/src/main/java/org/freedu/composecalculator/CalculatorViewModel.kt
@@ -8,4 +8,91 @@ import androidx.lifecycle.ViewModel
 class CalculatorViewModel:ViewModel() {
     var state by mutableStateOf(CalculatorState())
         private set
+
+    fun onAction(actions: CalculatorActions){
+        when(actions){
+            CalculatorActions.Calculate -> performCalculation()
+            CalculatorActions.Clear -> state = CalculatorState()
+            CalculatorActions.Decimal -> enterDecimal()
+            CalculatorActions.Delete -> performDeletion()
+            is CalculatorActions.Number -> enterNumber(actions.number)
+            is CalculatorActions.Operation -> enterOperation(actions.operation)
+        }
+    }
+
+    private fun performCalculation() {
+        val number1 = state.number1.toDoubleOrNull()
+        val number2 = state.number2.toDoubleOrNull()
+        if(number1 != null && number2!=null){
+            val result = when(state.operation){
+                CalculatorOperation.Add -> number1+number2
+                CalculatorOperation.Divide -> number1/number2
+                CalculatorOperation.Multiply -> number1*number2
+                CalculatorOperation.Subtract -> number1-number2
+                null->return
+            }
+
+            state = state.copy(
+                number1 = result.toString().take(15),
+                number2 = "",
+                operation = null
+            )
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        if(state.number1.isNotBlank()){
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun performDeletion() {
+        when{
+            state.number2.isNotBlank() -> state = state.copy(
+                number2 = state.number2.dropLast(1)
+            )
+            state.operation != null ->state = state.copy(
+                operation = null
+            )
+            state.number1.isNotBlank() -> state = state.copy(
+                number1 = state.number1.dropLast(1)
+            )
+        }
+    }
+
+    private fun enterDecimal() {
+        if(state.operation == null && !state.number1.contains(".") && state.number1.isNotBlank()){
+            state = state.copy(
+                number1 = state.number1 + "."
+            )
+            return
+        }
+        if(!state.number2.contains(".") && state.number2.isNotBlank()){
+            state = state.copy(
+                number2 = state.number2 + "."
+            )
+            return
+        }
+    }
+
+    private fun enterNumber(number: Int) {
+        if(state.operation == null){
+            if(state.number1.length>=MAX_NUM_LENGTH){
+                return
+            }
+            state = state.copy(
+                number1 = state.number1 + number
+            )
+            return
+        }
+        if (state.number2.length>= MAX_NUM_LENGTH){
+            return
+        }
+        state = state.copy(
+            number2 = state.number2 + number
+        )
+    }
+    companion object{
+        private const val MAX_NUM_LENGTH = 8
+    }
 }
--- app/src/main/java/org/freedu/composecalculator/MainActivity.kt
+++ app/src/main/java/org/freedu/composecalculator/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.freedu.composecalculator.ui.theme.ComposeCalculatorTheme
+import org.freedu.composecalculator.ui.theme.MediumGray
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +25,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ComposeCalculatorTheme {
-
+                val viewModel = viewModel<CalculatorViewModel>()
+                val state = viewModel.state
+                val buttonSpacing = 8.dp
+                Calculator(
+                    state = state,
+                    onAction = viewModel::onAction,
+                    buttonSpacing = buttonSpacing,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MediumGray)
+                        .padding(16.dp)
+                    )
             }
         }
     }
